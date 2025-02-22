f30353bf53c9b9ec2a193f73d80818e0ed3e09fb
olavelnan <olavelnan@gmail.com>
2025-02-22 16:01:03 +0100

    Remove unnecessary process.env definition in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/spill/",
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
   build: {
     rollupOptions: {
       output: {
