25a268bce3d8d80a38ad1ca2e6ccfcd911d21e18
olavelnan <olavelnan@gmail.com>
2025-02-25 12:28:53 +0100

    Update base path in vite.config.js to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/spill/",
+  base: "/",
   plugins: [react()],
   build: {
     rollupOptions: {
